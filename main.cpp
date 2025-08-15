61ebd2629923d59a71a499711d4d6521f82a1d7a
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-15 23:10:05 +0800

    mathematics day c++

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main() {
-    int file_size; //Snake Case
-    int FileSize;  // Pascal Case
-    int fileSize; // Camel Case
-    int iFileSize; // Hungarian Notation
+    int x=10;
+    int y=3;
+    int z= x+y;
+    std::cout<< z;
     return 0;
 
 
