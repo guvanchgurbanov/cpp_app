177917ff74025be5d3d87d20c4d546ffa56242c0
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-18 11:47:39 +0800

    My Practice cpp program easy way

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
+     //Declare a variable and initialize to 0
 
-    double sales =95000;
-    cout <<"Sales:$" <<sales<< endl;
-
-    const double stateTaxRate =.04;
-    double stateTax=sales * stateTaxRate;
-    cout<<"State Tax:$"<<stateTax<<endl;
-
-    double countyTaxRate =.02;
-    double countytax=sales * countyTaxRate;
-    cout<<"County Tax:$"<<countytax<<endl;
-
-    double totalTax= stateTax+ countytax;
-    cout<<"Total Tax:$"<<totalTax;
+     intx= 0；
 
     return 0;
 }
