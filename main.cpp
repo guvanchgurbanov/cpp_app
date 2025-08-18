4a2af6aaa0a67ec08c46b0827812f7fe6a36b3bb
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-18 11:47:39 +0800

    My Practice cpp program enter a valuar

--- main.cpp
+++ main.cpp
@@ -2,19 +2,10 @@
 using namespace std;
 int main() {
 
-    double sales =95000;
-    cout <<"Sales:$" <<sales<< endl;
-
-    const double stateTaxRate =.04;
-    double stateTax=sales * stateTaxRate;
-    cout<<"State Tax:$"<<stateTax<<endl;
-
-    double countyTaxRate =.02;
-    double countytax=sales * countyTaxRate;
-    cout<<"County Tax:$"<<countytax<<endl;
-
-    double totalTax= stateTax+ countytax;
-    cout<<"Total Tax:$"<<totalTax;
+    cout<<"Enter a valur";
+    double value;
+    cin >>value;
+    cout  <<value;
 
     return 0;
 }
