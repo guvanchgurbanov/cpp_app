483f8181a1eb6db2fa52e8678a90ce43c720e900
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-19 13:28:37 +0800

    fundamentals date program Random_Numbers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
-
 int main() {
-     //Declare a variable and initialize to 0
-
-     intx= 0;
+    long elapesedScenonds= time(0); //Jan 1 1970
+    srand (6);
+    int number= rand();
+    cout << elapesedScenonds;
 
     return 0;
 }
