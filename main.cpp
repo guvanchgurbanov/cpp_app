4712bacdab08747118521fc5e41a89578fb99d1e
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-21 16:03:24 +0800

    Random Numbers Cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 int main() {
-    short minValue = 1;
-    short maxValue = 6;
 
-    srand(time(0));
-
-    short first = rand() % (maxValue - minValue + minValue);
-    short second = rand() % (maxValue - minValue + minValue);
-
-    cout << first << ", " << second << endl;
-
+    int number= 1000000;
+    short another=number;
+    cout<<another;
     return 0;
 }
