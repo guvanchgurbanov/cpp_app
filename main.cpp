cc25de24af6d9202c4fcc05e340120ef69767abb
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-26 23:41:33 +0800

    Working with Arrays day practic

--- main.cpp
+++ main.cpp
@@ -3,26 +3,8 @@
 using namespace std;
 
 int main() {
-    string street;
-    cout <<"street";
-    getline (cin,street);
-
-    string city;
-     cout<< "City: ";
-    getline(cin,city);
-
-
-    string state;
-    cout<<"State:";
-    getline(cin,state);
-
-
-    string postalCode;
-    cout<<"Postal Code: ";
-    getline(cin,postalCode);
-
-    cout<<street<< endl
-        << city << "," <<state<< "," <<postalCode <<endl;
+    int numbers[5];
+    cout << numbers[1];
 
     return 0;
 
