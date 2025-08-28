8b26c80fed7328866042159c553e1b41116b30f0
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-28 16:41:38 +0800

    Buliding programtype Conversion 1.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 int main() {
     int x = 1;
-    double y = 2.2;
-    double z = x + y;
+    double y = 2.0;
+    int z = x + static_cast <int> (y);
     cout << z;
-
 
     return 0;
 
