2d52760bc25976b36325864978fcb16d0530abf7
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-29 23:14:49 +0800

    Comparison Operators false cout tybe

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main() {
-    int x=10;
-    double y= 5;
-    bool result = x == y ;
-    cout << boolalpha << result;
+    char first = 'a';
+    char second = 'A';
+    bool result = first == second;
+    cout <<result;
 
     return 0;
 
