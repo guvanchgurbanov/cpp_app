e39f80ed6a5a92faacca98e81e09dc1a9e4a23f7
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-31 21:30:59 +0800

    sum of digits of the numbers 1000-9999

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ int main() {
     int a {};
     cin >> a;
 
-    int birinjiSifr = a / 100;
-    int ortaSifr = ( a / 10) % 10;
+    int birinjiSifr = a / 1000;
+    int ikinjiSifr = ( a / 100) % 1000;
+    int ucunjiSifir = (a /10) %100;
     int sonkySifr = a % 10;
 
-    int sifrlerinJemi = birinjiSifr + sonkySifr + ortaSifr;
+    int sifrlerinJemi = birinjiSifr + ikinjiSifr + ucunjiSifir+ sonkySifr;
     cout << sifrlerinJemi;
 
     return 0;
