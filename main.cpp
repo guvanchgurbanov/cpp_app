aa56eecaa30a38f8b4107bb552db2f442f0c38b3
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-08-31 22:56:40 +0800

    Fix: Finding sum of digits of the numbers 1000-9999

--- main.cpp
+++ main.cpp
@@ -6,13 +6,12 @@ int main() {
     int x {};
     cin >> x;
 
-    int birinjiSifr = x / 10000;
-    int ikinjiSifr = ( x / 1000) % 10;
-    int ucunjiSifir = (x / 100)%10;
-    int dordinjiSifir = (x / 10)%10;
+    int birinjiSifr = x / 1000;
+    int ikinjiSifr = ( x / 100) %10;
+    int ucunjiSifir = (x / 10)%10;
     int sonkySifr = x % 10;
 
-    int sifrlerinJemi = birinjiSifr + ikinjiSifr + ucunjiSifir+dordinjiSifir+ sonkySifr;
+    int sifrlerinJemi = birinjiSifr + ikinjiSifr + ucunjiSifir + sonkySifr;
     cout << sifrlerinJemi;
 
     return 0;
