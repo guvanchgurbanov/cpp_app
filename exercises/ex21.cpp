899652740d7e82ba6d59340ebd55707b6f4ff1fa
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-09-05 23:57:01 +0800

    Exercise Cpp Program solution  20-22

---
+++ exercises/ex21.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Guga on 05-Sep-25.
+//#include <iostream>
+
+using namespace std;
+int main(){
+    int L ;
+    cin >> L;
+    int  answer = L * 100 ;
+    cout <<"Jogaby " << answer << " sntimetr " << endl;
+
+    return 0;
+}
---
+++ exercises/ex22.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Guga on 05-Sep-25.
+//#include <iostream>
+
+using namespace std;
+int main(){
+    int M ;
+    cin >> M;
+    int  answer = M * 1000 ;
+    cout <<"Jogaby " << answer << " Kg " << endl;
+
+    return 0;
+}
