4879d9c2bbcb3b6d1dcd90edcedc8aac14f0cb87
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-09-15 23:53:57 +0800

    ex 29 new

--- exercises/ex29.cpp
+++ exercises/ex29.cpp
@@ -1,26 +1,27 @@
 //
 // Created by Guga on 14-Sep-25.
-//#include <iostream>
+#include <iostream>
 
 using namespace std;
+
 int main() {
+    cout << "Gonuburchlygyn taraplaryny giriz: " << endl;
     int a,b ;
     cin >> a >> b;
-    // "Gonuburclugyn her tarapyna nace kwadrat yerlesyandigini tapmak";
+
+    cout << "Kwadratyn tarapyny giriz: " << endl;
     int c;
-    cin >> c ;
+    cin >> c;
 
-    // Jemi kwadratlaryň sany
-    int jemiKwadrat = a * b;
-    int goniburcluk = c * c;
-    int x = (a/c ) * (b/c);
+    int gonuburclygynMeydany = a * b;
+    int inedordulinMeydany = c * c;
 
+    if (gonuburclygynMeydany < inedordulinMeydany) {
+        cout << "Bu inedordul gunuburclyga syganogahou!!! Name kellan agyryamy?" << endl;
+    } else {
+        cout << "Ulanylman galan bolegi: " << gonuburclygynMeydany % inedordulinMeydany  << endl;
+    }
 
-    cout << x << jemiKwadrat << endl ;
-    cout << "Galan meydan" << goniburcluk << endl;
-
-    int y = jemiKwadrat - (x * jemiKwadrat);
-    cout << "Remaining area: " << y << endl;
 
     return 0;
 }
