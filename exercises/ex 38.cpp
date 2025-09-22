72e8b5690c20a80ae73febe630639ea8a49bd9ae
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-09-22 23:18:44 +0800

    Cpp Ex 38

---
+++ exercises/ex 38.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Guga on 22-Sep-25.
+// #include <iostream>
+
+using namespace std;
+int main() {
+
+    int yyl, ay, gun;
+    cout << "Sene giriz (Yyl, Ay, Gun): ";
+    cin >> yyl >> ay >> gun;
+
+    cout << yyl <<"."<< ay  <<"."<< gun + 1;
+
+    return 0 ;
+}
--- main.cpp
+++ main.cpp
@@ -2,50 +2,19 @@
 
 using namespace std;
 int main() {
-    int g;
-    cout << "1-Kilogram , 2-Milligram , 3-Gram , 4-Tonna ,5-Sentner  Haysyny isleseniz saylan:";
-    cin >> g;
 
-    if ( g < 1 || 5 < g ) {
-        cout << "Nadogry girish";
-        return 1;
-    }
+    int a;
+    cout << " Birinji san girzir";
+    cin >> a;
 
-    double c;
-    cout << " sangiriz: " ;
-    cin >>c;
+    int b;
+    cout << "Ikinji sany girzir";
+    cin >>b;
 
-    double Jogaby;
-
-
-    switch (g) {
-        case 1:
-            Jogaby = c / 1 ;
-            cout << c << " Killogram ";
-            break;
-
-        case 2:
-            Jogaby = c / 1000 ;
-            cout << c << " Milligram ";
-            break;
-
-        case 3:
-            Jogaby = c * 0.001;
-            cout << c << " Gram ";
-            break;
-
-        case 4:
-            Jogaby = c * 1000;
-            cout << c << " Tonna ";
-            break;
-
-        case 5:
-            Jogaby = c * 100;
-            cout << c << " Sentner ";
-            break;
-    }
-
-    cout << " Jogaby: " << Jogaby  << " Killogram" ;
+    if ( a == b) {
+        cout << "0" << endl;
+    }else
+        cout
 
     return 0 ;
 }
