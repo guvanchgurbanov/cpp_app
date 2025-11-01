f69c1707e89a54036d63efd435880f71b756a340
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-11-01 18:12:03 +0800

    ex C++ work with For While

---
+++ exercises/this C++ work with For.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Guga on 01-Nov-25.
+//#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10 ; i++)
+        cout << i << endl ;
+
+    return 0;
+}
