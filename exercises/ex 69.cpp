002e5e35a0883dbf143ed142d426d66c2bca80c3
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-11-12 15:11:51 +0800

    ex 69

---
+++ exercises/ex 69.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Guga on 04-Nov-25.
+//#include <iostream>
+
+using namespace std;
+
+int main() {
+    int suyjinbahsy;
+    cout << "suyjin klesinin bahasyny giriz:";
+    cin >> suyjinbahsy;
+
+    for (int i = 1; i <=10 ; i++) {
+        cout << i << " kg suyjinin bahasy: " ;
+        cout << suyjinbahsy * i  << "manat"<< endl;
+    }
+    cout << endl;
+    return 0;
+}
