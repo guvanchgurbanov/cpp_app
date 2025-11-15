75ab6bca01d50180e214d03257571f50fd5ed833
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-11-15 23:24:36 +0800

    ex 72

---
+++ exercises/ex 72.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Guga on 07-Nov-25.
+//#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cout <<"A number enter: ";
+    cin >> a;
+    cout <<"B number enter: ";
+    cin >> b;
+
+    int total = 0;
+    cout << "Numbers: ";
+    for (int i = a; i <= b ; i++) {
+        cout <<i;
+        total +=i;
+    }
+    cout << endl;
+    cout << "Total number: " << total << endl;
+
+    return 0;
+}
