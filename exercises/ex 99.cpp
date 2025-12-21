d3f42da4511bf5d5d36fcc7934969b5838a5224b
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2025-12-21 21:32:42 +0800

    ex 99  2

--- exercises/ex 99.cpp
+++ exercises/ex 99.cpp
@@ -1,3 +1,21 @@
 //
 // Created by Guga on 20-Dec-25.
-//
+//#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "N sany girizin : ";
+    cin >> n ;
+
+    int K = 0;
+    int gymmatlyk = 1;
+
+    while (gymmatlyk * 3 < n) {
+        gymmatlyk = gymmatlyk * 3;
+        K++;
+    }
+    cout << K;
+
+    return 0;
+}
