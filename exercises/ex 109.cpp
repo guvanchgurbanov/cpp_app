13499202bdf31a4515296407dd3de897ebcc6b28
Guvanch Gurbanov <gurbanovguga02@gmail.com>
2026-01-13 14:14:01 +0800

    ex 109

---
+++ exercises/ex 109.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Guga on 12-Jan-26.
+//#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "N sany girizin : ";
+    cin >> n ;
+
+    bool yonekey = true;
+
+    for (int i = 2; i <= n - 1; i++) {
+        if (n % i == 0) {
+            yonekey = false;
+            break;
+        }
+    }
+
+    if (yonekey)
+        cout << "TRUE";
+    else
+        cout << "FALSE";
+    return 0;
+}
